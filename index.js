af6a6875015a85fa7dcaecaf227167db57d7e474
Rime <rimeislam672@gmail.com>
2024-01-24 02:52:01 +0600

    owner site data

--- index.js
+++ index.js
@@ -27,7 +27,61 @@ try {
         const result = await rentCollection.insertOne(taskitem);
         res.send(result);
       })
-
+    app.get('/rents', async (req, res) => {
+        const result = await rentCollection.find().toArray();
+        res.send(result);
+      });
+    app.get('/userrents', async (req, res) => {
+        try {
+            console.log(req.query.email);
+            let query = {};
+            if (req.query?.email) {
+                console.log(req.query?.email)
+                query = { email: req.query.email };
+            }
+            const result = await rentCollection.find(query).toArray();
+            console.log(result)
+            res.send(result);
+        } catch (error) {
+            console.error(error);
+            res.status(500).send("Internal Server Error");
+        }
+    });
+    app.delete('/userrents/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await rentCollection.deleteOne(query);
+        res.send(result);
+      })
+    app.get('/userrents/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id)};
+        const result = await rentCollection.findOne(query);
+        res.send(result);
+      })
+      app.put('/userrents/:id', async(req, res) => {
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)};
+        const options = { upsert: true };
+        const updatedItem = req.body;
+        const item ={
+          $set: {
+            name: updatedItem.name,
+            address: updatedItem.address,
+            city: updatedItem.city,
+            bedroom: updatedItem.bedroom,
+            bathroom: updatedItem.bathroom,
+            size: updatedItem.size,
+            rent: updatedItem.rent,
+            phn: updatedItem.phn,
+           date: updatedItem.date,
+           notes: updatedItem.notes,
+           image: updatedItem.image,
+          }
+        };
+        const result = await rentCollection.updateOne(filter, item, options);
+        res.send(result);
+      })
 
 // await client.connect();
 // await client.db("admin").command({ ping: 1 });
